0e368253123bde90e5eed1d0c2476b18ef306411
Enzo1603 <96661578+Enzo1603@users.noreply.github.com>
2024-12-09 23:01:29 +0100

    markdown_to_html function

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,37 @@
 use std::process::Command;
 
 fn main() {
-    let status = Command::new("mkdir")
-        .arg("neues_verzeichnis")
+    // let status = Command::new("mkdir")
+    //     .arg("neues_verzeichnis")
+    //     .status()
+    //     .expect("Konnte mkdir nicht ausführen");
+
+    // if status.success() {
+    //     println!("Verzeichnis wurde erfolgreich erstellt.");
+    // } else {
+    //     eprintln!("Fehler beim Erstellen des Verzeichnisses.");
+    // }
+
+    markdown_to_html();
+}
+
+fn markdown_to_html() {
+    let status = Command::new("pandoc")
+        .arg("VPS-Setup.md")
+        .arg("-o")
+        .arg("output_dark.html")
+        .arg("--standalone")
+        .arg("--embed-resources")
+        .arg("--template")
+        .arg("github-markdown-template.html")
+        .arg("--css")
+        .arg("github-markdown-dark.css")
         .status()
-        .expect("Konnte mkdir nicht ausführen");
+        .expect("Could not execute pandoc");
 
     if status.success() {
-        println!("Verzeichnis wurde erfolgreich erstellt.");
+        println!("Conversion successful.");
     } else {
-        eprintln!("Fehler beim Erstellen des Verzeichnisses.");
+        eprintln!("Conversion failed.");
     }
 }
