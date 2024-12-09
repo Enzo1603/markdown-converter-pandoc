03bb06a074a0a445799ffb270684f269c03b9728
Enzo1603 <96661578+Enzo1603@users.noreply.github.com>
2024-12-09 23:19:47 +0100

    Read theme

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,10 @@
+use colored::*;
 use std::io;
+use std::io::Write;
 use std::process::Command;
 
 fn main() {
-    loop {
-        let mut theme = String::new();
-        println!("Enter theme (light, dark, both): ");
-        io::stdin()
-            .read_line(&mut theme)
-            .expect("Failed to read line");
-
-        let theme = match Theme::from_input(&theme) {
-            Ok(theme) => theme,
-            Err(e) => {
-                eprintln!("{}", e);
-                continue;
-            }
-        };
-
-        break;
-    }
+    get_theme();
 
     markdown_to_html();
 }
@@ -37,6 +23,30 @@ impl Theme {
             "both" => Ok(Theme::Both),
             _ => Err("Invalid theme".to_string()),
         }
+    }
+}
+
+fn get_theme() -> Theme {
+    loop {
+        let mut theme = String::new();
+        print!("{}", "Enter theme (light, dark, both): ".blue());
+
+        // Flush the buffer
+        io::stdout().flush().unwrap();
+
+        io::stdin()
+            .read_line(&mut theme)
+            .expect("Failed to read line");
+
+        let theme = match Theme::from_input(&theme) {
+            Ok(theme) => theme,
+            Err(e) => {
+                eprintln!("{}\n", e.red());
+                continue;
+            }
+        };
+
+        return theme;
     }
 }
 
